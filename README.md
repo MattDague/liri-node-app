9ed6d413b768407c7f86f48452bbb0a273c53201
MattDague <mattdague@gmail.com>
2019-07-25 23:13:22 -0400

    finished version with readME fully written, link to video demo in readME

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # liri-node-app
+
+Introduction 
+---------------------------------------------
+
+Welcome to the Liri App! I have created every aspect of this app from start to finish for your entertainment! The purpose of Liri is to allow you to search for information on your favorite movies, songs and bands. It even has a selection for you if you cant decide! 
+
+The catch is that all of the functionality is in the command line. From the user input to the display results there is no need for an additional page to load.
+
+Development
+---------------------------------------------
+
+The app is created in a way that it responds to the input from the user and takes that data to make a request through Axios or the Spotify API for a response. The user will use node.js to input the actions for a concert, movie or track search and the app will take that command and call the corresponding function. The search the app chooses is chosen using a series of if statements.
+
+The user must also provide an additional argument in the command line which acts at their search criteria. 
+
+Each function for search immediately calls another function called generateInput. What this function does is take the users argument at the 3 index of their input. The function then uses a for loop to format their text into an array so that it can be sent as part of the query URL.
+
+The search functions for track and movie are set up so that if the array the generateInput function tries to generate is empty, default values are given to the search. If an input is properly generated a request is sent out using the users search criteria. If data is returned it is then displayed to the user in the console. 
+
+The response displays are all pulled from the objects from the response data. The concert search does use functionality from momentjs to display its date in a much more readable format.
+
+If the user chooses to go the do-what-it-says route the respective function is called. From this the app accesses the local filesystem and checks the random.txt document. That text then split into an array. The 0 index is used to pick the action and the 1 index then fed as the full input to the query URL for search purposes.
+
+How To Use
+-------------------------------------------
+
+To begin you must go to the directory of the app in your terminal or console. Once you are in the directory you must use Node Package Manager to install the following packages:
+
+- Axios
+- Moment.js
+- Spotify-Api
+
+Once installed you can start using the app. To being every search through Liri you must type in the following:
+
+    node liri.js 
+
+What is typed in afterwards may vary but these two arguments are required to start it. Now you have a choise to make and you have 4 options to choose from:
+
+
+To search for a trach you must type in:
+
+    spotify-this-song <The name of the song>
+
+To search for a movie you must type in:
+
+    movie-this <The name of the movie>
+
+To search for a concert by a band you must type in:
+
+    concert-this <The name of the band>
+
+Or if you want Liri to take the wheel you must type:
+
+    do-what-it-says 
+
+
+
+For example a search for a movie would look like the following code:
+
+node liri.js movie-this Saving Private Ryan
+
+
+Each search will return you with an abundance of information on that given topic!
+
+
+Technologies Used
+----------------------------------------
+- Node.js
+- Javascript
+- Axios
+- NPM 
+- Spotify API
+- Moment.js
+- OMDB API
+- Bands In Town API
+- dotenv
+
+
+
+
+GitHub Repository: https://github.com/MattDague/liri-node-app
+
+Video Demonstration: https://youtu.be/XZ0wgsGOyWk
--- liri.js
+++ liri.js
@@ -1,23 +1,25 @@
 require("dotenv").config();
+//requirements for calls
 var keys = require("./keys.js");
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 var fs = require("fs");
+//variable for all CLI inputs
 var args = process.argv;
-var action = process.argv[2]
+// pulls action from CLI
+var action = process.argv[2];
+//array for gathering search terms
 var input = [];
-var inputFull = ""
+//variable for final search input
+var inputFull = "";
 
 //======================================//
 // Axios/OMDB
 
-//grabs action from CLI
-
-
+//if statements for determining which action gets called
 if (action == "movie-this") {
-
   movieThis()
 };
 
@@ -25,14 +27,11 @@ if (action == "concert-this") {
   concertThis();
 };
 
-
 if (action == "spotify-this-song") {
-
   spotifyThis();
 };
 
 if (action == "do-what-it-says") {
-
   doWhatItSays();
 };
 
@@ -40,11 +39,11 @@ if (action == "do-what-it-says") {
 // do what it says function
 
 function doWhatItSays() {
-
+// reads from random.txt file
   fs.readFile("./random.txt", "utf8", function (err, data) {
-
+// splits the text from the file
     var dataArr = data.split(",");
-
+//if statements checking for which function to call
     if (dataArr[0] == "spotify-this-song") {
       inputFull = dataArr[1].toString();
       spotifyThis();
@@ -53,13 +52,11 @@ function doWhatItSays() {
       inputFull = dataArr[1].toString();
       movieThis();
     }
-    if (dataArr[0] == "concert-this") {
+    else if (dataArr[0] == "concert-this") {
       inputFull = dataArr[1].toString();
       concertThis();
-    }
-    // console.log(dataArr);
-
-  })
+    };
+  });
 };
 
 
@@ -67,10 +64,10 @@ function doWhatItSays() {
 //spotify function
 
 function spotifyThis() {
-  //if left blank it defaults to "The Sign" by Ace of Base
 
   generateInput();
 
+ //if left blank it defaults to "The Sign" by Ace of Base
   if (inputFull.length == 0) {
 
     spotify
@@ -92,7 +89,7 @@ function spotifyThis() {
 
     // spotify search query
     spotify.search({ type: 'track', query: inputFull }, function (err, data) {
-      console.log(inputFull)
+      
       if (err) {
         return console.log('Error occurred: ' + err);
       }
@@ -101,8 +98,8 @@ function spotifyThis() {
       console.log("Artist: " + data.tracks.items[0].artists[0].name);
       console.log("Album: " + data.tracks.items[0].album.name);
       console.log("Spotify link to track: " + data.tracks.items[0].external_urls.spotify);
-    })
-  }
+    });
+  };
 
 };
 
@@ -112,29 +109,28 @@ function spotifyThis() {
 function concertThis() {
   
   generateInput();
-
+//url for bands in town search
   var queryUrl = "https://rest.bandsintown.com/artists/" + inputFull + "/events?app_id=codingbootcamp"
 
-
+//axis request to bands in town
   axios.get(queryUrl)
     .then(
       function (response) {
         console.log("\nHere are their next 5 shows: ");
-
+        // loop to display the 5 next concerts found
         for (var i = 0; i < 5; i++) {
           console.log("");
-          console.log(response.data[i].venue.name);
-          console.log(response.data[i].venue.city + ", " + response.data[i].venue.country);
-          // console.log(response.data[i].datetime);
-
+          console.log("Venue: " + response.data[i].venue.name);
+          console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+          // time conversion using momentjs
           var time = response.data[i].datetime
           var format = "YYYY-MM-DD, hh:mm:ss"
           var convertedDate = moment(time, format);
-          console.log(convertedDate.format("MM/DD/YYYY"))
+          console.log("Date: " + convertedDate.format("MM/DD/YYYY"))
 
-        }
+        };
       });
-}
+};
 
 
 
@@ -149,7 +145,6 @@ function movieThis() {
     
     inputFull = "mr+nobody"
   }
-  //if a movie is put into the console
  
  
 
@@ -161,28 +156,20 @@ function movieThis() {
       function (response) {
 
         //console logs all movie data retrieved
-        console.log(response.data.Title);
-        console.log(response.data.Year);
+        console.log("Movie title: " + response.data.Title);
+        console.log("Year: " + response.data.Year);
         console.log("IMDB rating: " + response.data.Ratings[0].Value);
         console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
-        console.log(response.data.Country);
-        console.log(response.data.Language);
-        console.log(response.data.Plot);
-        console.log(response.data.Actors);
+        console.log("Country :"+ response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
       }
     ).catch(function (error) {
-      if (error.response) {
-
-        //console.logs for errors
-        console.log(error.response.data);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
+      //logging error
       console.log(error.config);
     });
-}
+};
 
 
 
@@ -194,4 +181,4 @@ for (var i = 3; i < args.length; i++) {
   input.push(args[i])
   inputFull = input.join(" ")
 }
-}
+};
