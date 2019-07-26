a48c1625420dea55e08c7364558791994835ac52
MattDague <mattdague@gmail.com>
2019-07-25 20:07:31 -0400

    mostly final version, reworked all actions into functions, do-what-it-says calls those functions, removed all input generators and put them into their own universal function

--- liri.js
+++ liri.js
@@ -5,121 +5,115 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 var fs = require("fs");
-
+var args = process.argv;
 var action = process.argv[2]
+var input = [];
+var inputFull = ""
 
 //======================================//
 // Axios/OMDB
 
-//grabs action from cli
+//grabs action from CLI
+
+
 if (action == "movie-this") {
 
   movieThis()
-}
-
+};
 
 if (action == "concert-this") {
   concertThis();
 };
 
 
-if (action == "spotify-this") {
+if (action == "spotify-this-song") {
 
   spotifyThis();
+};
 
+if (action == "do-what-it-says") {
 
+  doWhatItSays();
+};
 
-  if (action == "do-what-it-says") {
-    doWhatItSays();
+//===============================================
+// do what it says function
 
-  
+function doWhatItSays() {
 
-  }
+  fs.readFile("./random.txt", "utf8", function (err, data) {
 
-  //===============================================
-  // do what it says function
+    var dataArr = data.split(",");
 
-  function doWhatItSays() {
-    fs.readFile("./random.txt", "utf8", function (error, data) {
-      console.log("node " + "liri.js " + data);
-
-
-      // var dataArr = data.split(",");
-      // if (dataArr[0]= spotify-this) ()
-      // input= data arr[1];
-      // inputFull = input
-
-
-
-      console.log(dataArr);
-
-    })
-  };
-
-
-  //================================================
-  //spotify function
-
-  function spotifyThis() {
-    //if left blank it defaults to "The Sign" by Ace of Base
-    if (process.argv[3] === undefined) {
-
-      spotify
-        .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
-        .then(function (data) {
-          console.log("Track: " + data.name);
-          console.log("Artist: " + data.artists[0].name);
-          console.log("Album: " + data.album.name);
-          console.log("Spotify link to track: " + data.external_urls.spotify);
-        })
-        .catch(function (err) {
-          console.error('Error occurred: ' + err);
-        });
-
+    if (dataArr[0] == "spotify-this-song") {
+      inputFull = dataArr[1].toString();
+      spotifyThis();
     }
+    else if (dataArr[0] == "movie-this") {
+      inputFull = dataArr[1].toString();
+      movieThis();
+    }
+    if (dataArr[0] == "concert-this") {
+      inputFull = dataArr[1].toString();
+      concertThis();
+    }
+    // console.log(dataArr);
 
-    // if content is entered
-    else {
-      // sets variable for user input
-      var args = process.argv;
-      //array to hold track name
-      var track = []
-      //loop to format track nameinput for search
-      for (var i = 3; i < args.length; i++) {
-        track.push(args[i]);
-        var musicQuery = track.join(" ");
+  })
+};
 
 
-      }
-      // spotify search query
-      spotify.search({ type: 'track', query: musicQuery }, function (err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-        //display for track, artist, album and a link to track on spotify
-        console.log("Track: " + data.tracks.items[0].name);
-        console.log("Artist: " + data.tracks.items[0].artists[0].name);
-        console.log("Album: " + data.tracks.items[0].album.name);
-        console.log("Spotify link to track: " + data.tracks.items[0].external_urls.spotify);
+//================================================
+//spotify function
+
+function spotifyThis() {
+  //if left blank it defaults to "The Sign" by Ace of Base
+
+  generateInput();
+
+  if (inputFull.length == 0) {
+
+    spotify
+      .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
+      .then(function (data) {
+        console.log("Track: " + data.name);
+        console.log("Artist: " + data.artists[0].name);
+        console.log("Album: " + data.album.name);
+        console.log("Spotify link to track: " + data.external_urls.spotify);
       })
-    }
+      .catch(function (err) {
+        console.error('Error occurred: ' + err);
+      });
+
   }
+
+  // if content is entered
+  else {
+
+    // spotify search query
+    spotify.search({ type: 'track', query: inputFull }, function (err, data) {
+      console.log(inputFull)
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      //display for track, artist, album and a link to track on spotify
+      console.log("Track: " + data.tracks.items[0].name);
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Album: " + data.tracks.items[0].album.name);
+      console.log("Spotify link to track: " + data.tracks.items[0].external_urls.spotify);
+    })
+  }
+
 };
 
 //=============================================
 // Bands In Town
 
 function concertThis() {
-  var args = process.argv;
-  var artist = []
+  
+  generateInput();
 
-
-  for (var i = 3; i < args.length; i++) {
-    artist.push(args[i])
-    var artistInput = artist.join("+")
-
-  }
-  var queryUrl = "https://rest.bandsintown.com/artists/" + artistInput + "/events?app_id=codingbootcamp"
+  var queryUrl = "https://rest.bandsintown.com/artists/" + inputFull + "/events?app_id=codingbootcamp"
 
 
   axios.get(queryUrl)
@@ -143,27 +137,24 @@ function concertThis() {
 }
 
 
+
 //=================================
 //function for movie-this
 function movieThis() {
+
+  generateInput();
+
   //if nothing is input into the console after movie-this, the default search is mr. nobody
-  if (process.argv[3] == undefined) {
-    movieInput = "mr+nobody"
+  if (inputFull.length == 0) {
+    
+    inputFull = "mr+nobody"
   }
   //if a movie is put into the console
-  else {
-    var args = process.argv;
-    var movie = [];
-
-    //pushes user input into array and creates the input for the search
-    for (var i = 3; i < args.length; i++) {
-      movie.push(args[i])
-      var movieInput = movie.join(" ")
-    };
-  }
+ 
+ 
 
   // url for omdb api search
-  var movieUrl = "http://www.omdbapi.com/?t=" + movieInput + "&y=&plot=short&apikey=trilogy"
+  var movieUrl = "http://www.omdbapi.com/?t=" + inputFull + "&y=&plot=short&apikey=trilogy"
 
   axios.get(movieUrl)
     .then(
@@ -191,4 +182,16 @@ function movieThis() {
       }
       console.log(error.config);
     });
+}
+
+
+
+// function for generating input
+function generateInput (){
+
+//pushes user input into array and creates the input for the search
+for (var i = 3; i < args.length; i++) {
+  input.push(args[i])
+  inputFull = input.join(" ")
+}
 }
